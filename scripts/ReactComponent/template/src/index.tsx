5dc138875a45a9b7f06d2809e91230a3f5de452c
pengjiasheng <pengjiasheng@cvte.com>
2020-07-15 15:23:37 +0800

    refactor: modify react component template

--- scripts/ReactComponent/template/src/index.tsx
+++ scripts/ReactComponent/template/src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import {IAppProps, IAppState} from './externals';
 
-export default class extends React.Component<IAppProps, IAppState> {
-  public render() {
-    return (
-      <div>Hello world</div>
-    );
-  }
-}
+interface IAppProps {}
+
+const Component: React.FC<IAppProps> = props => {
+  return (
+    <div>Hello world</div>
+  );
+};
+
+Component.displayName = 'Component';
+
+export default Component;
